180433af9006c32e4beaca425f54f05a1551f920
Yunkyu Lee <gtv1523411@gmail.com>
2019-08-19 19:37:34 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { purple, orange, red } from '@material-ui/core/colors';
+import { purple, orange, red, grey } from '@material-ui/core/colors';
 import MapPage from './MapPage';
 import Main from './Main';
 
 const theme = createMuiTheme({
   palette: {
     primary: red,
-    secondary: {
-      main: '#f44336',
-    },
+    secondary: purple,
+    third: grey,
   },
   status: {
     danger: orange[500],
